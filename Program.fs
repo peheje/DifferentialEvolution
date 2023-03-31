b63c8de2c8d167f1098d8cdce703fe71fdffb330
peheje <peterhj@gmail.com>
2023-03-31 17:18:22 +0200

    compress a bit

--- Program.fs
+++ Program.fs
@@ -1,15 +1,16 @@
 ﻿open Problems
 
-let random = System.Random.Shared
 let sw = System.Diagnostics.Stopwatch.StartNew ()
 
+let random = System.Random.Shared
 let rand () = random.NextDouble()
-let randRange min max = rand () * (max - min) + min
+let randomFloatRange max min = random.NextDouble() * (max - min) + min
+let randomInt max = random.Next(max)
 
 type Agent = { xs: float array; score: float }
 
 let sample agents =
-    let i = random.Next(agents |> Array.length)
+    let i = randomInt(agents |> Array.length)
     agents[i].xs
 
 let print = 1000
@@ -18,12 +19,12 @@ let argsize = 100
 let min, max = -10.0, 10.0
 let generations = 10_000
 let popsize = 200
-let crossoverOdds () = randRange 0.1 1.0
-let mutateOdds () = randRange 0.2 0.95
+let crossoverOdds () = randomFloatRange 0.1 1.0
+let mutateOdds () = randomFloatRange 0.2 0.95
 let clamp x = System.Math.Clamp(x, min, max)
 
 let createAgent () =
-    let xs = Array.init argsize (fun _ -> randRange min max)
+    let xs = Array.init argsize (fun _ -> randomFloatRange min max)
     { xs = xs; score = optimizer xs }
 
 let pool = Array.init popsize (fun _ -> createAgent ())
@@ -31,19 +32,14 @@ let pool = Array.init popsize (fun _ -> createAgent ())
 let mate pool agent =
     let crossover = crossoverOdds ()
     let mutate = mutateOdds ()
-    let x0 = sample pool
-    let x1 = sample pool
-    let x2 = sample pool
+    let x0, x1, x2 = sample pool, sample pool, sample pool
 
     let trial =
         Array.init argsize (fun j ->
-            if rand () < crossover then
-                (x0[j] + (x1[j] - x2[j]) * mutate) |> clamp
-            else
-                agent.xs[j])
+            if rand () < crossover then x0[j] + (x1[j] - x2[j]) * mutate |> clamp
+            else agent.xs[j])
 
     let trialScore = optimizer trial
-
     if trialScore < agent.score then
         { xs = trial; score = trialScore }
     else
