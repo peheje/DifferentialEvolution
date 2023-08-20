ced39ddaee7097022e9547cfba8d739b7e1bf191
peheje <peterhj@gmail.com>
2023-08-20 17:53:03 +0200

    parallel stream

--- kotlin/untitled/src/main/kotlin/Main.kt
+++ kotlin/untitled/src/main/kotlin/Main.kt
@@ -1,20 +1,28 @@
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.random.Random
+import kotlin.time.measureTime
 
 const val min = -10.0
 const val max = 10.0
-const val argsize = 100
-const val popsize = 100
-const val generations = 1000
-const val print = 1000
-val optimizer = ::rastrigin
+const val argsize = 1000
+const val popsize = 200
+const val generations = 10000
+const val print = 100
+val optimizer = ::f1
 
 fun main(args: Array<String>) {
 
+    measureTime {
+        algorithm()
+    }.let { println("Elapsed: ${it.inWholeMilliseconds}ms") }
+
+}
+
+private fun algorithm() {
     var pool = List(popsize) { createAgent() }
 
-    repeat(generations) {g ->
+    repeat(generations) { g ->
         if (g % print == 0) {
             val scores = pool.map { it.score }
             println("Generation $g")
@@ -22,12 +30,13 @@ fun main(args: Array<String>) {
             println("Min ${scores.min()}")
         }
 
-        val next = pool.map { mate(pool, it) }
-        pool = next
+        val next = pool.parallelStream().map { mate(pool, it) }
+        pool = next.toList()
     }
 
     val best = pool.minBy { it.score }
-    println("Generation be")
+    println("Generation best ${best.xs.contentToString()}")
+    println("score: ${best.score}")
 }
 
 data class Agent(val xs: DoubleArray, val score: Double)
@@ -40,7 +49,7 @@ fun mate(pool: List<Agent>, agent: Agent): Agent {
     val x1 = pool.random().xs
     val x2 = pool.random().xs
 
-    val trial = DoubleArray(argsize) {i ->
+    val trial = DoubleArray(argsize) { i ->
         if (Random.nextDouble() < crossoverOdds) {
             (x0[i] + (x1[i] - x2[i]) * mutateOdds).coerceIn(min, max)
         } else {
@@ -49,18 +58,20 @@ fun mate(pool: List<Agent>, agent: Agent): Agent {
     }
 
     val trialScore = optimizer(trial)
-    if (trialScore < agent.score) {
-        return Agent(xs = trial, score = trialScore)
+    return if (trialScore < agent.score) {
+        Agent(xs = trial, score = trialScore)
     } else {
-        return agent.copy()
+        agent.copy()
     }
 }
 
+fun f1(xs: DoubleArray) = xs.sumOf { it * it }
+
 fun rastrigin(xs: DoubleArray): Double {
-    return xs.sumOf { (it * it) - (10.0 * cos(2.0 * PI * it)) }.let { 10 * xs.size * it }
+    return xs.sumOf { (it * it) - (10.0 * cos(2.0 * PI * it)) }.let { 10.0 * xs.size * it }
 }
 
 fun createAgent(): Agent {
     val xs = DoubleArray(argsize) { Random.nextDouble(min, max) }
-    Agent(xs = xs, score = optimizer(xs))
+    return Agent(xs = xs, score = optimizer(xs))
 }
